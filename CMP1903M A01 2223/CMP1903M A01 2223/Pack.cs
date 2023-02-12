cec17b3f11b8e3d094c7dcccea23fc4377627c67
Alex Dickinson <P215280@outlook.com>
2023-02-12 16:39:29 +0000

    Initial Push

---
+++ CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Pack
+    {
+        List<Card> pack;
+        
+        public Pack()
+        {
+            //Initialise the card pack here
+            pack = new List<Card>();
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= 13; j++)
+                {
+                    pack.Add(new Card(j, i));
+                }
+            }
+        }
+
+        public void overhandShuffle()
+        {
+            //Overhand shuffle the pack
+            Random rnd = new Random();
+            int n = pack.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                Card value = pack[k];
+                pack[k] = pack[n];
+                pack[n] = value;
+            }
+        }
+        
+        public void riffleShuffle()
+        {
+            //Riffle shuffle the pack
+            Random rnd = new Random();
+            int n = pack.Count;
+            int k = rnd.Next(n / 2);
+            int l = rnd.Next(n / 2);
+            while (n > 1)
+            {
+                n--;
+                Card value = pack[k];
+                pack[k] = pack[l];
+                pack[l] = value;
+            }
+        }
+        
+        public void fisherYatesShuffle()
+        {
+            //Fisher-Yates shuffle the pack
+            Random rnd = new Random();
+            int n = pack.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                Card value = pack[k];
+                pack[k] = pack[n];
+                pack[n] = value;
+            }
+        }
+        public void shuffleCardPack(int typeOfShuffle)
+        {
+            //Shuffles the pack based on the type of shuffle
+            switch (typeOfShuffle)
+            {
+                case 1:
+                    //Overhand Shuffle
+                    overhandShuffle();
+                    break;
+                case 2:
+                    //Riffle Shuffle
+                    riffleShuffle();
+                    break;
+                case 3:
+                    //Fisher-Yates Shuffle
+                    fisherYatesShuffle();
+                    break;
+            }
+        }
+        public static Card deal(List<Card> pack)
+        {
+            //Deals a card from the pack
+            Card dealtCard = pack[pack.Count - 1];
+            pack.RemoveAt(pack.Count - 1);
+            return dealtCard;
+        }
+        public static List<Card> dealCards(int amount, Pack newPack)
+        {
+            //Deals the number of cards specified by 'amount'
+            List<Card> dealtCards = new List<Card>();
+            for (int i = 1; i <= amount; i++)
+            {
+                dealtCards.Add(deal(newPack.pack));
+            }
+            return dealtCards;
+        }
+    }
+}
---
+++ CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new pack of cards
+            Pack pack = new Pack();
+            
+            //Ask the user how many cards they want to deal
+            Console.WriteLine("How many cards do you want to deal?");
+            
+            int numCards = 0;
+            while (numCards < 1 || numCards > 52) //Error Trapping the input
+            {
+                try //Error trapping to see if a number is entered
+                {
+                    numCards = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 52");
+                    numCards = 0;
+                }
+            }
+
+            //Ask the user what shuffle to use
+            Console.WriteLine("Which shuffle do you want to use? (1) Overhand, (2) Riffle, (3) Fisher-Yates");
+            int shuffle = 0;
+            while (shuffle < 1 || shuffle > 3) //Error Trapping the input
+            {
+                try //Error trapping to see if a number is entered
+                {
+                    shuffle = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    shuffle = 0;
+                }
+            }
+            
+            //Shuffle the pack
+            pack.shuffleCardPack(shuffle);
+
+            //Deal the cards and add them to dealt cards
+            List<Card> dealtCards = Pack.dealCards(numCards, pack);
+
+            //Print the dealt cards
+            string[] suits = new string[] { "Clubs", "Diamonds", "Hearts", "Spades" }; //This is just to make the output look nicer
+            string[] values = new string[] { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+            
+            Console.WriteLine("Dealt cards:");
+            foreach (Card card in dealtCards)
+            {
+                Console.WriteLine(values[card.Value -1] + " of " + suits[card.Suit-1]);
+            }
+        }
+    }
+}
